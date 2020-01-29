3c7f37f6f1073afcc4d986a2f1de4d480cf633a9
kwlahr <kwlahr@gmail.com>
2020-01-28 17:19:11 -0700

    added api/workouts/:id get route to server.js and added comments to server routes

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
+//telling the following routes to use api.js when "app" is called
 app.use(require("./public/api.js"), function(req, res) {
   if (err) {
     throw err;
@@ -27,14 +28,39 @@ app.use(require("./public/api.js"), function(req, res) {
   }
 });
 
+//route to access index.html
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
+//route to access workouts database
+app.get("/api/workouts", function(req, res) {
+  // res.sendFile(path.join(__dirname, "index.html"));
+  Workout.find({}, function(err, workouts){res.json(workouts)});
+});
+
+app.get("/api/workouts/range", function(req, res) {
+  Workout.find({}, function(err, workouts){res.json(workouts)});
+});
+
+//get route for individual workouts
+app.get("/api/workouts/:id", function(req, res) {
+  // Workout.find({_id: id(`${req.params.id}`)}, function(err, workouts){res.json(workouts)});
+  console.log(req.params.id);
+  Workout.find({}, function(err, workouts){res.json(workouts)});
+});
+
+//updates instances of workouts based on id
+app.put("/api/workouts/:id", function(req, res) {
+  console.log(req.params.id);
+})
+
+//route to exercise.html
 app.get("/exercise", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/exercise.html"));
 });
 
+//post request creates a new instance of a workout in the database
 app.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
     .then(dbWorkout => {
@@ -46,10 +72,12 @@ app.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+//route to access stats.html
 app.get("/stats", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/stats.html"));
 });
 
+//sets port to lacalhost:3000
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
